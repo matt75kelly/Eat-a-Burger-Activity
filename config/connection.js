5aff8e45f186a819cc1a845b93ae05e87fd292ee
Yuendemu <matthew75kelly@gmail.com>
2018-08-17 11:45:00 -0500

    Setup server.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
-const mysql = require("mysql");
 require("dotenv").config();
+const mysql = require("mysql");
+
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: 3306,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
