04a14d7f254d88ac8a71de36140f2d27085f551b
Yuendemu <matthew75kelly@gmail.com>
2018-08-18 21:56:22 -0500

    heroku deployment with database

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,24 @@
 require("dotenv").config();
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+    });
+};
 connection.connect(err=>{
     if(err){
         console.log(`DB Connection: ${err}`);
     }
     else{
-        console.log(`Connected to database: ${process.env.DB_NAME}`);
         console.log(`\nConnected as id: ${connection.threadId}`);
     }
 });
